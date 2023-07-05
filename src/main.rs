b2367e4f481b010cecb58853d0700740b9a1154a
DosangGu <ddos0418@gmail.com>
2023-07-05 13:21:00 +0000

    Args 준비

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![allow(unused)]
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
+
+/// Simple program to greet a person
+/// ```rust
+/// #[derive(Parser, Debug)]
+/// #[command(author, version, about, long_about = None)]
+/// struct Args {
+///     /// Name of the person to greet
+///     #[arg(short, long)]
+///     name: String,
+///
+///     /// Number of times to greet
+///     #[arg(short, long, default_value_t = 1)]
+///     count: u8,
+/// }
+/// ```
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {}
