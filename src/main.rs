e921ba0fadd7934ce1c235eacab33bdb83ee2f8b
DosangGu <ddos0418@gmail.com>
2023-07-06 12:45:55 +0000

    remove_bom 구현

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![allow(unused)]
+
 use clap::Parser;
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Read, Write};
+use std::path::Path;
 
 fn main() {
     let args = Args::parse();
@@ -22,3 +26,22 @@ fn main() {
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {}
+
+/// remove utf-8 BOM mark of given file
+fn remove_bom<P: AsRef<Path>>(path: P) -> std::io::Result<()> {
+    let mut reader = BufReader::new(File::open(&path)?);
+
+    let mut buf = [0; 3];
+    reader.read_exact(&mut buf)?;
+
+    let mut contents = Vec::new();
+    if buf != [0xEF, 0xBB, 0xBF] {
+        contents.extend_from_slice(&buf);
+    }
+    reader.read_to_end(&mut contents)?;
+
+    let mut writer = BufWriter::new(File::create(path)?);
+    writer.write_all(&contents)?;
+
+    Ok(())
+}
