db5a33637c557a43e81d42b88ba28da5157e0146
DosangGu <ddos0418@gmail.com>
2023-07-07 13:02:55 +0000

    불필요 제거

--- src/main.rs
+++ src/main.rs
@@ -13,26 +13,10 @@ fn main() {
     let args = Args::parse();
 }
 
-/// Simple program to greet a person
-/// ```rust
-/// #[derive(Parser, Debug)]
-/// #[command(author, version, about, long_about = None)]
-/// struct Args {
-///     /// Name of the person to greet
-///     #[arg(short, long)]
-///     name: String,
-///
-///     /// Number of times to greet
-///     #[arg(short, long, default_value_t = 1)]
-///     count: u8,
-/// }
-/// ```
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     path: Option<String>,
     #[arg(short, long, required = false)]
-    recursive: bool,
-    #[arg(long, required = false)]
     option: Option<String>,
 }
