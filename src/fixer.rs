883200cad6a934376837be5161764b36fe37a71f
DosangGu <ddos0418@gmail.com>
2023-07-14 12:57:24 +0000

    add_bom 버그 수리

--- src/fixer.rs
+++ src/fixer.rs
@@ -1,8 +1,8 @@
-﻿use std::error::Error;
+﻿use crate::checker;
+use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufReader, BufWriter, Read, Write};
 use std::path::{Path, PathBuf};
-
 use tempfile::NamedTempFile;
 
 /// remove utf-8 BOM mark of given file
@@ -31,18 +31,22 @@ pub fn add_bom(path: &PathBuf) -> Result<bool, Box<dyn Error>> {
     let mut reader = get_file_reader(path)?;
 
     let mut buf = vec![0; BOM.len()];
+
     reader.read_exact(&mut buf)?;
 
     if &buf == BOM {
         return Ok(false);
     }
 
+    reader.read_to_end(&mut buf)?;
+    if checker::is_buf_utf8(&buf)? == false {
+        return Ok(false);
+    }
+
     let mut temp_file = NamedTempFile::new()?;
     {
         let mut writer = BufWriter::new(&mut temp_file);
-        writer.write_all(BOM)?; // Write BOM
         writer.write_all(&buf)?; // Write the already read bytes
-        io::copy(&mut reader, &mut writer)?; // Copy the rest of the file
     }
     temp_file.persist(&path)?;
     println!("Added BOM to {}", path.display());
